347b5538f39e1e6f49a6be3c116f2f4953e71a87
Veronica Carnero <vcarnero@gmail.com>
2016-02-25 21:17:33 -0700

    more changes calling the capitalize method
    [#114536265]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -3,7 +3,7 @@ n = "this is a string to practice with"
 puts n
 puts n.upcase
 puts n.capitalize
-puts n.gsub("string","'string'")
+puts n.gsub("string","'string'").capitalize
 puts n.gsub(n,"The string '#{n}' has 33 characters")
 puts n.reverse
 puts n.replace("practice practice practice!")
