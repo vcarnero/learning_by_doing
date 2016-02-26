0ebaeeece66320c012c81a4156ce675070f8967c
Veronica Carnero <vcarnero@gmail.com>
2016-02-25 20:54:03 -0700

    my code was not DRY, so I had to change line 6&7
    [#114536265]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -3,7 +3,7 @@ n = "this is a string to practice with."
 puts n
 puts n.upcase
 puts n.capitalize
-puts n.gsub(n,"This is a 'string' to practice with")
-puts n.gsub(n,"The string 'this is a string to practice with' has 33 characters")
+puts n.gsub("string","'string'")
+puts n.gsub(n,"The string '#{n}' has 33 characters")
 puts n.reverse
 puts n.replace("practice practice practice!")
