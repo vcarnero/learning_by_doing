e20fbb48ec1f6089c2c0596525fb19da6fd8c757
Veronica Carnero <vcarnero@gmail.com>
2016-02-25 20:16:00 -0700

    ammending sytax space errors.
    [#114536265]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -3,7 +3,7 @@ n = "this is a string to practice with."
 puts n
 puts n.upcase
 puts n.capitalize
-puts n.gsub(n,"This is a'string'to practice with")
+puts n.gsub(n,"This is a 'string' to practice with")
 puts n.length
 puts n.reverse
 puts n.replace("practice practice practice")
