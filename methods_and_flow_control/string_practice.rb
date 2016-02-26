bcebd01b5b175532f533120b7f01a8362691cb36
Veronica Carnero <vcarnero@gmail.com>
2016-02-25 20:32:47 -0700

    maybe the additional space will correct the error.
    [#114536265]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -4,6 +4,6 @@ puts n
 puts n.upcase
 puts n.capitalize
 puts n.gsub(n,"This is a 'string' to practice with")
-puts n.gsub(n,"The string 'This is a string to practice with' has 33 characters")
+puts n.gsub(n,"the string'this is a string to practice with' has 33 characters")
 puts n.reverse
 puts n.replace("practice practice practice")
