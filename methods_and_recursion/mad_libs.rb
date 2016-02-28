a7b0ceacf650d43fed3889a516a0c2172209d619
Veronica Carnero <vcarnero@gmail.com>
2016-02-28 09:22:27 -0700

    mad_libs
    [#114624247]

---
+++ methods_and_recursion/mad_libs.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+
+#A vacation is when you take a trip to some (adjective) place with your (adjective) family.
+# Usually you go to some place that is near a/an (noun) or up on a/an (noun).
+# A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun).
+# I like to spend my time (verb ending in "ING") or (verb ending in "ing").
+#  When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit around (verb ending in "ing").
+# Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body).
+# My family is going to go to (the) (a place), and I will practice (verb ending in "ing").
+# Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number) hours every day all year making enough (plural noun) to pay for the vacation.
+
+#to do
+
+
+#Give me an adjective
+print 'Give me an adjective:'
+adjective_1 = gets.chomp
+
+#Give me another adjective
+print 'Give me another adjective:'
+adjective_2 = gets.chomp
+
+#Give me a noun
+print 'Give me a noun:'
+noun_1 = gets.chomp
+#Give me another noun
+print 'Give me another noun:'
+noun_2 = gets.chomp
+
+#Give me a plural noun
+print 'Give me a plural noun:'
+plural_noun_1 = gets.chomp
+
+#Give me a game
+print 'Give me a game:'
+game = gets.chomp
+
+#Give me another plural noun
+print 'Give me another plural noun:'
+plural_noun_2 = gets.chomp
+
+#Give me a verb ending in "ing"
+print 'Give me a verb ending in "ing":'
+verb_ing_1 = gets.chomp
+
+#Give me another verb ending in "ing"
+print 'Give me another verb ending in "ing":'
+verb_ing_2 = gets.chomp
+
+#Give me a plural noun
+print 'Give me a plural noun:'
+plural_noun_3 = gets.chomp
+
+#Give me a verb ending in "ing"
+print 'Give me a verb ending in "ing":'
+verb_ing_3 = gets.chomp
+
+#Give me a noun
+print 'Give me a noun:'
+noun_3 = gets.chomp
+
+#Give me a plant
+print 'Give me a plant:'
+plant = gets.chomp
+
+#Give me a part of the body
+print 'Give me a part of the body:'
+body_part = gets.chomp
+
+#Give me a place
+print 'Give me a place:'
+place = gets.chomp
+
+#Give me a verb ending in "ing"
+print 'Give me another verb ending in "ing":'
+verb_ing_4 = gets.chomp
+
+#Give me an adjective
+print 'Give me an adjective:'
+adjective_3 = gets.chomp
+
+#Give me a number
+print 'Give me a number:'
+number = gets.chomp
+
+
+#Give me a plural noun
+print 'Give me another plural noun:'
+plural_noun_4 = gets.chomp
+
+print 'Here is your madlib: '
+
+puts "A vacation is when you take a trip to some #{adjective_1} place with your #{adjective_2} family.
+Usually you go to some place that is near a/an #{noun_1} or up on a/an #{noun_2}."
+puts "A good vacation place is one where you can ride #{plural_noun_1} or play #{game} or go hunting for #{plural_noun_2}."
+puts "I like to spend my time #{verb_ing_1} or #{verb_ing_2}. "
+puts "When parents go on a vacation, they spend their time eating three #{plural_noun_3} a day, and fathers play golf, and mothers sit around #{verb_ing_3}."
+puts "Last summer, my little brother fell in a/an #{noun_3} and got poison #{plant} all over his #{body_part}."
+puts "My family is going to go to (the) #{place}, and I will practice #{verb_ing_4}."
+puts "Parents need vacations more than kids because parents are always very #{adjective_3} and because they have to work #{number} hours every day all year making enough #{plural_noun_4} to pay for the vacation."
+
+
+
+
