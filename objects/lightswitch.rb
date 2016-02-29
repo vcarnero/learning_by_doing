d5be284c26f3786eed6ec90d9b39a203f1e4ec6f
Veronica Carnero <vcarnero@gmail.com>
2016-02-29 14:40:36 -0700

    practicing

---
+++ objects/lightswitch.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+#create an object light switch that turns on/off at certain times of the day
+#@5pm everyday the light turns on
+#@11 pm it turns off
+#@6am the light turns on
+
+class Lightswitch
+  def on (time_of_day)
+    if time_of_day == five
+      return "The light is on"
+    elsif return "The light is off"
+    end
+  end
+
