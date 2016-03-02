b55ed1c302d13ad788207948be9cae1bdf9ece16
Veronica Carnero <vcarnero@gmail.com>
2016-03-01 22:14:51 -0700

    insert "."
    [#114624265]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -12,7 +12,7 @@
     puts "#{number} bottle of beer on the wall, #{number} bottle of beer."
     puts "Take one down and pass it around, no more bottles of beer on the wall\n\n"
     puts "No more bottles of beer on the wall, no more bottles of beer."
-    puts "Go to the store and buy some more, 99 bottles of beer on the wall"
+    puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   elsif number == 2
     puts "2 bottles of beer on the wall, 2 bottles of beer."
     puts "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
