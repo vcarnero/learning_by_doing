40d170d605da6971787cbb84d3de509ef4f838ae
Veronica Carnero <vcarnero@gmail.com>
2016-03-01 22:10:31 -0700

    changed conditional statement
    [#114624265]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -8,7 +8,7 @@
 
 
 99.downto(1) do |number|
-  if number -1 == 0
+  if number == 1
     puts "#{number} bottle of beer on the wall, #{number} bottle of beer."
     puts "Take one down and pass it around, no more bottles of beer on the wall\n\n"
     puts "No more bottles of beer on the wall, no more bottles of beer."
@@ -16,9 +16,6 @@
   elsif number == 2
     puts "2 bottles of beer on the wall, 2 bottles of beer."
     puts "Take one down and pass it around, 1 bottle of beer on the wall.\n\n"
-  elsif number == nil
-    puts "No more bottles of beer on the wall, no more bottles of beer."
-    puts "Go to the store and buy some more, 99 bottles of beer on the wall\n\n"
   else
     puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
     puts "Take one down and pass it around, #{number -1 } bottles of beer on the wall.\n\n"
