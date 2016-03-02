cb5d7f2e7875f6ed007bae5165c20bd414970de7
Veronica Carnero <vcarnero@gmail.com>
2016-03-01 22:16:02 -0700

    another '.'
    [#114624265]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -10,7 +10,7 @@
 99.downto(1) do |number|
   if number == 1
     puts "#{number} bottle of beer on the wall, #{number} bottle of beer."
-    puts "Take one down and pass it around, no more bottles of beer on the wall\n\n"
+    puts "Take one down and pass it around, no more bottles of beer on the wall.\n\n"
     puts "No more bottles of beer on the wall, no more bottles of beer."
     puts "Go to the store and buy some more, 99 bottles of beer on the wall."
   elsif number == 2
