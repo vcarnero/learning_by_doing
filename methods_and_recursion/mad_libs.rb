8616128d98f918cdd6d25f2e21275a2bed69f8e7
Veronica Carnero <vcarnero@gmail.com>
2016-03-01 21:08:48 -0700

    made space similar to mad lib spacing example
    [#114624247]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -88,15 +88,24 @@ number = gets.chomp
 print 'Give me another plural noun:'
 plural_noun_4 = gets.chomp
 
-print 'Here is your madlib:/n/n'
+print "Here is your madlib:\n\n"
 
-puts "A vacation is when you take a trip to some #{adjective_1} place with your #{adjective_2} family. Usually you go to some place that is near a/an #{noun_1} or up on a/an #{noun_2}."
-puts "A good vacation place is one where you can ride #{plural_noun_1} or play #{game} or go hunting for #{plural_noun_2}."
-puts "I like to spend my time #{verb_ing_1} or #{verb_ing_2}."
-puts "When parents go on a vacation, they spend their time eating three #{plural_noun_3} a day, and fathers play golf, and mothers sit around #{verb_ing_3}."
-puts "Last summer, my little brother fell in a/an #{noun_3} and got poison #{plant} all over his #{body_part}."
-puts "My family is going to go to (the) #{place}, and I will practice #{verb_ing_4}."
-puts "Parents need vacations more than kids because parents are always very #{adjective_3} and because they have to work #{number} hours every day all year making enough #{plural_noun_4} to pay for the vacation."
+puts "A vacation is when you take a trip to some #{adjective_1} place"
+puts "with your #{adjective_2} family. Usually you go to some place"
+puts "that is near a/an #{noun_1} or up on a/an #{noun_2}."
+puts "A good vacation place is one where you can ride #{plural_noun_1}"
+puts "or play #{game} or go hunting for #{plural_noun_2}. I like"
+puts "to spend my time #{verb_ing_1} or #{verb_ing_2}."
+puts "When parents go on a vacation, they spend their time eating"
+puts "three #{plural_noun_3} a day, and fathers play golf, and mothers"
+puts "sit around #{verb_ing_3} Last summer, my little brother"
+puts "fell in a/an #{noun_3} and got poison #{plant} all"
+puts "over his #{body_part}. My family is going to go to (the)"
+puts "#{place},and I will practice #{verb_ing_4}.Parents"
+puts "need vacations more than kids because parents are always very"
+puts "#{adjective_3} and because they have to work #{number} "
+puts "hours every day all year making enough #{plural_noun_4} to pay"
+puts" for the vacation."
 
 
 
