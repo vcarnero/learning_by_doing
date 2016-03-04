fb50021f5d591530a34bb5066abf2d2260c264b0
Veronica Carnero <vcarnero@gmail.com>
2016-03-04 11:02:51 -0700

    ...

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -4,7 +4,7 @@ numbers_array = [1,2,3,4,5,6,7,8,9,10]
 
 
 #1...2...3...4...5...6...7...8...9...10...
-puts "#{numbers_array.join('...')}(...)"
+puts "#{numbers_array.join('...')}..."
 
 #T-10, 9, 8, 7, 6, 5, 4, 3, 2, 1...  BLASTOFF!
 puts "T-#{numbers_array.reverse.join(', ')}... BLASTOFF!"
