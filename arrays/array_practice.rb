4bd44370bad9282a6b7f5b37dae8b010709a145e
Veronica Carnero <vcarnero@gmail.com>
2016-03-04 10:51:57 -0700

    array practice took a long time
    [#115039755]

--- arrays/array_practice.rb
+++ arrays/array_practice.rb
@@ -19,13 +19,33 @@ puts "The first element is #{numbers_array.first}"
 puts "The third element is #{numbers_array[2]}"
 
 #The element with an index of 3 is 4
+puts "The element with an index of 3 is #{numbers_array[3]}"
 
 #The second from last element is 9
-#The first four elements are '1, 2, 3, 4'
-#If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]
-#If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]
-# If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]
-#Only the elements [9, 10] are > 8.
+puts "The second from last element is #{numbers_array[8]}"
 
+#The first four elements are '1, 2, 3, 4'
+puts "The first four elements are #{numbers_array[0...4]}"
+
+#If we delete 5, 6 and 7 from the array, we're left with [1,2,3,4,8,9,10]
+numbers_array.delete_at(4)
+# [1,2,3,4,6,7,8,9,10]
+numbers_array.delete_at(4)
+# [1,2,3,4,7,8,9,10]
+numbers_array.delete_at(4)
+# [1,2,3,4,7,8,9,10]
+puts "If we delete 5, 6 and 7 from the array, we're left with '#{numbers_array}'"
+
+#If we add 5 at the beginning of the array, we're left with [5,1,2,3,4,8,9,10]
+puts "If we add 5 at the beginning of the array, we're left with #{numbers_array.insert(0,5)}"
+
+# If we add 6 at the end of the array, we're left with [5,1,2,3,4,8,9,10,6]
+puts "If we add 6 at the end of the array, we're left with #{numbers_array.insert(8,6)}"
+
+#Only the elements [9, 10] are > 8.
+puts "Only the elements [#{numbers_array[6..7].join(', ')}] are > 8."
+
+# IF we remove all the elements then the length of the array is 0
+puts "If we remove all the elements, then the length of the array is #{numbers_array.clear.length}"
 
 
