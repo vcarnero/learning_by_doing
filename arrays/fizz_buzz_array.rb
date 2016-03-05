8cd28e09b5d49c0121e7500b22876bc101f97da7
Veronica Carnero <vcarnero@gmail.com>
2016-03-05 11:08:29 -0700

    typos

---
+++ arrays/fizz_buzz_array.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+def fizzbuzz(n)
+  (1..100).to_a.each do |i|
+    if i % 3 ==0 && i % 5 ==0
+      puts 'FizzBuzz'
+    elsif i % 3 == 0
+      puts 'Fizz'
+    elsif i % 5 == 0
+      puts 'Buzz'
+    else
+      puts i
+    end
+  end
+end
+
+fizzbuzz(100)
