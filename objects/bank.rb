964c941428711a0abf991da5d12590b7a69d91f4
Veronica Carnero <vcarnero@gmail.com>
2016-03-06 14:28:57 -0700

    code that object in Ruby
    [#114643719]

---
+++ objects/bank.rb
@@ -0,0 +1,23 @@
+# class Bank
+# method that withdraws money from your bank account
+# method that deposits money to your bank account
+# method that gives you the total amount of money in your bank
+
+
+class Bank
+  def initialize(amount_in_bank = 0.0)
+    @balance = amount_in_bank
+  end
+
+  def deposit(amount)
+    @balance = amount + @balance
+  end
+
+  def withdrawal(cash_out)
+    return @balance - cash_out
+  end
+
+  def total
+    @balance = total
+  end
+end
