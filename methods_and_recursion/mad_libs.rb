0b463d8a38e7b2e70b97c3365cc75d0d03544a7e
Veronica Carnero <vcarnero@gmail.com>
2016-03-23 20:59:23 -0600

    [#114624247] added ask_question method

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -1,92 +1,29 @@
 #!/usr/bin/env ruby
 
-#A vacation is when you take a trip to some (adjective) place with your (adjective) family.
-# Usually you go to some place that is near a/an (noun) or up on a/an (noun).
-# A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun).
-# I like to spend my time (verb ending in "ING") or (verb ending in "ing").
-#  When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit around (verb ending in "ing").
-# Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body).
-# My family is going to go to (the) (a place), and I will practice (verb ending in "ing").
-# Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number) hours every day all year making enough (plural noun) to pay for the vacation.
+def ask_question(part_of_speech)
+  print "Give me #{part_of_speech}:"
+  gets.chomp
+end
 
-#to do
-
-
-#Give me an adjective
-print 'Give me an adjective:'
-adjective_1 = gets.chomp
-
-#Give me another adjective
-print 'Give me another adjective:'
-adjective_2 = gets.chomp
-
-#Give me a noun
-print 'Give me a noun:'
-noun_1 = gets.chomp
-#Give me another noun
-print 'Give me another noun:'
-noun_2 = gets.chomp
-
-#Give me a plural noun
-print 'Give me a plural noun:'
-plural_noun_1 = gets.chomp
-
-#Give me a game
-print 'Give me a game:'
-game = gets.chomp
-
-#Give me another plural noun
-print 'Give me another plural noun:'
-plural_noun_2 = gets.chomp
-
-#Give me a verb ending in "ing"
-print 'Give me a verb ending in "ing":'
-verb_ing_1 = gets.chomp
-
-#Give me another verb ending in "ing"
-print 'Give me another verb ending in "ing":'
-verb_ing_2 = gets.chomp
-
-#Give me a plural noun
-print 'Give me a plural noun:'
-plural_noun_3 = gets.chomp
-
-#Give me a verb ending in "ing"
-print 'Give me a verb ending in "ing":'
-verb_ing_3 = gets.chomp
-
-#Give me a noun
-print 'Give me a noun:'
-noun_3 = gets.chomp
-
-#Give me a plant
-print 'Give me a plant:'
-plant = gets.chomp
-
-#Give me a part of the body
-print 'Give me a part of the body:'
-body_part = gets.chomp
-
-#Give me a place
-print 'Give me a place:'
-place = gets.chomp
-
-#Give me a verb ending in "ing"
-print 'Give me another verb ending in "ing":'
-verb_ing_4 = gets.chomp
-
-#Give me an adjective
-print 'Give me an adjective:'
-adjective_3 = gets.chomp
-
-#Give me a number
-print 'Give me a number:'
-number = gets.chomp
-
-
-#Give me a plural noun
-print 'Give me another plural noun:'
-plural_noun_4 = gets.chomp
+adjective_1 = ask_question("an adjective")
+adjective_2 = ask_question("another adjective")
+noun_1 = ask_question("a noun")
+noun_2 = ask_question("another noun")
+plural_noun_1 = ask_question("a plural noun")
+game = ask_question("a game")
+plural_noun_2 = ask_question("another plural noun")
+verb_ing_1 = ask_question('a verb ending in "ing"')
+verb_ing_2 = ask_question('another verb ending in "ing"')
+plural_noun_3 = ask_question("a plural noun")
+verb_ing_3 = ask_question('a verb ending in "ing"')
+noun_3 = ask_question("a noun")
+plant = ask_question("a plant")
+body_part = ask_question("a part of the body")
+place = ask_question("a place")
+verb_ing_4 = ask_question('another verb ending in "ing"')
+adjective_3 = ask_question("an adjective")
+number = ask_question("a number")
+plural_noun_4 = ask_question("another plural noun")
 
 print "Here is your madlib:\n\n"
 
